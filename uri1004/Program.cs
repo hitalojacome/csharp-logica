6087d679d9ccd39a81ba641e8ba580fad77de312
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-17 13:54:50 -0300

    Produto Simples

---
+++ uri1004/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uri1004 {
+    class Program {
+        static void Main(string[] args) {
+
+            int A, B, produto;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            produto = A * B;
+
+            Console.WriteLine("PROD = " + produto);
+            
+        }
+    }
+}
