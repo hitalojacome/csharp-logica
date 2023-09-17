729d7034c5a71051ebe93da2c6d8e77c9ff26228
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-17 17:32:08 -0300

    Esfera

---
+++ uri1011/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uri1011{
+    class Program {
+        static void Main(string[] args) {
+
+            double pi, radius, volume;
+
+            pi = 3.14159;
+            radius = double.Parse(Console.ReadLine());
+
+            volume = 4.0 / 3.0 * pi * Math.Pow(radius, 3.0);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3"));
+            
+        }
+    }
+}
