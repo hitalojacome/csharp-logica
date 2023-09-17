67663061a0b50e919c8d29fa4e311019a60d0a7c
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-17 16:16:15 -0300

    Salário

---
+++ uri1008/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uri1008 {
+    class Program {
+        static void Main(string[] args) {
+            
+            int idEmployed, hour;
+            double valueHour, salary;
+
+            idEmployed = int.Parse(Console.ReadLine());
+            hour = int.Parse(Console.ReadLine());
+            valueHour = double.Parse(Console.ReadLine());
+
+            salary = valueHour * hour;
+
+            Console.WriteLine("Employed number = " + idEmployed);
+            Console.WriteLine("Salary = U$" + salary.ToString("F2"));
+
+        }
+    }
+}
