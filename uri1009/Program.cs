85fa319195f64f251b82efeff2be5c94f497fcbb
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-17 16:38:37 -0300

    Salário com Bônus

---
+++ uri1009/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+
+namespace uri1009 {
+        class Program {
+            static void Main(string[] args) {
+                
+                string name;
+                double fixSalary, totalSell, totalSalary;
+
+                name = Console.ReadLine();
+                fixSalary = double.Parse(Console.ReadLine());
+                totalSell = double.Parse(Console.ReadLine());
+
+                totalSalary = totalSell * 15.0 / 100 + fixSalary;
+
+                Console.WriteLine("TOTAL = R$" + totalSalary.ToString("F2"));
+
+            }
+        }        
+    }
