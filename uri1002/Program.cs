30566f49312a46d4d9eef037157e1200fa26986e
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-17 13:41:21 -0300

    uri1002

--- uri1002/Program.cs
+++ uri1002/Program.cs
@@ -8,7 +8,7 @@ namespace uri1002 {
 
             pi = 3.14159;
             raio = double.Parse(Console.ReadLine());
-            area = pi * Math.Pow(raio, 2.0);
+            area = pi * Math.Pow(raio, 2,0);
 
             Console.WriteLine("A=" + area.ToString("F4"));            
 
