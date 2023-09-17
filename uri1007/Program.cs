570194a5498eea01438ccff03baba011fa49470c
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-17 16:07:12 -0300

    Diferença

---
+++ uri1007/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace uri1007 {
+    class Program {
+        static void Main(string[] args) {
+
+            int A, B, C, D, difference;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            difference = A * B - C * D;
+
+            Console.WriteLine("DIFERENÇA = " + difference);
+            
+        }
+    }
+}
