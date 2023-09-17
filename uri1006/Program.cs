ba4808b2e67ddde78ce7ee13b4e2b3b60ec1ce79
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-17 16:00:57 -0300

    Média 2

---
+++ uri1006/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1006 {
+    class Program {
+        static void Main(string[] args) {
+
+            double a, b, c, media;
+
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+
+            media = ((a * 2.0) + (b * 3.0) + (c * 5.0)) / 10;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1"));
+
+        }
+    }
+}
