5cabef5cde42dc4d85d2138a1807acbadfa3715f
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-17 13:29:51 -0300

    Área do Circulo

---
+++ uri1002/Program.cs
@@ -0,0 +1,17 @@
+﻿﻿using System;
+
+namespace uri1002 {
+    class Program {
+        static void Main(string[] args) {
+
+            double pi, raio, area;
+
+            pi = 3.14159;
+            raio = double.Parse(Console.ReadLine());
+            area = pi * Math.Pow(raio, 2.0);
+
+            Console.WriteLine("A=" + area.ToString("F4"));            
+
+        }
+    }
+}
