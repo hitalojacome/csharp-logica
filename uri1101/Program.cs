b28bdf5ebac4fc45daf317bd0464ae94b621dfae
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-18 09:20:03 -0300

    Seqência de Números e Soma

--- uri1101/Program.cs
+++ uri1101/Program.cs
@@ -4,8 +4,31 @@ namespace uri1101 {
     class Program {
         static void Main(string[] args) {
 
-            
-            
+            string[] values = Console.ReadLine().Split(' ');
+            int m = int.Parse(values[0]);
+            int n = int.Parse(values[1]);
+
+            while (m > 0 && n > 0) {
+
+                int smaller = m;
+                int greater = n;
+
+                if (m > n) {
+                    smaller = n;
+                    greater = m;
+                }
+
+                int sum = 0;
+                for (int i = smaller; i <= greater; i++) {
+                    sum += i;
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine("Sum=" + sum);
+
+                values = Console.ReadLine().Split(' ');
+                m = int.Parse(values[0]);
+                n = int.Parse(values[1]);
+            }
         }
     }
 }
