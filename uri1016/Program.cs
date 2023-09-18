e6250017d5cc8800d7ae51a40edc107b597f4e1c
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-18 19:51:16 -0300

    Distância

---
+++ uri1016/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace uri1016 {
+    class Program {
+        static void Main(string[] args) {
+
+            int distance, time;
+
+            distance = int.Parse(Console.ReadLine());
+
+            time = distance * 2;
+
+            Console.WriteLine(time + " minutos");
+            
+        }
+    }
+}
