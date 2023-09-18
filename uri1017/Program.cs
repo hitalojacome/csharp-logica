9fb92d9338bf53f6f6607ee6702aa0d3804c1c79
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-18 19:55:14 -0300

    Gasto de Combustível

---
+++ uri1017/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace uri1017 {
+    class Program {
+        static void Main(string[] args) {
+
+            int time, velocity;
+            double distance, liters;
+
+            time = int.Parse(Console.ReadLine());
+            velocity = int.Parse(Console.ReadLine());
+
+            distance = velocity * time;
+            liters = distance / 12;
+
+            Console.WriteLine(liters.ToString("F3"));
+            
+        }
+    }
+}
