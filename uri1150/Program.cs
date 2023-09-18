a209582a624c2ccb55b259727c0f530cceaaf94d
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-18 12:24:19 -0300

    Fatorial Simples

---
+++ uri1150/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uri1150 {
+    class Program {
+        static void Main(string[] args) {
+
+            int x = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+
+            while (z <= x) {
+                z = int.Parse(Console.ReadLine());
+            }
+
+            int count = 1;
+            int sum = x;
+            do {
+                x += 1;
+                sum += x;
+                count += 1;
+            } while (sum <= z);
+
+            Console.WriteLine(count);
+            
+        }
+    }
+}
