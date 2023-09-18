8b89b63339310d17906e7525ff8ebf8ff5d67a29
HITALO JACOME <hjacome@websupply.com.br>
2023-09-18 08:08:39 -0300

    O Maior

---
+++ uri1013/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uri1013 {
+    class Program {
+        static void Main(string[] args) {
+            
+            int A, B, C, greatestAB, greatest;
+
+            string[] values = Console.ReadLine().Split(' ');
+            A = int.Parse(values[0]);
+            B = int.Parse(values[1]);
+            C = int.Parse(values[2]);
+
+            greatestAB = (A + B + Math.Abs(A - B)) / 2;
+            greatest = (greatestAB + C + Math.Abs(greatestAB - C)) / 2;
+
+            Console.WriteLine(greatest + " eh o maior!");
+
+        }
+    }
+}
