8941c8d7fa38dbff7489a244658cfca5cef89efe
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-18 11:39:06 -0300

    Somando Inteiros Consecutivos

---
+++ uri1149/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace uri1149 {
+    class Program {
+        static void Main(string[] args) {
+
+            int A, N, i, sum;
+
+            string[] values = Console.ReadLine().Split(' ');
+
+            A = int.Parse(values[0]);
+
+            i = 1;
+            N = int.Parse(values[i]);
+            while (N <= 0) {
+                i += 1;
+                N = int.Parse(values[i]);
+            }
+
+            sum = 0;
+            for (i = 0; i < N; i++) {
+                sum += A + i;
+            }
+
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
