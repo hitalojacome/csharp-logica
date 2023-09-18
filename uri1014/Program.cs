0991ce873098a985f868be3627eb8aee35740b4a
HITALO JACOME <hjacome@websupply.com.br>
2023-09-18 08:14:08 -0300

    Consumo

---
+++ uri1014/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1014 {
+    class Program {
+        static void Main(string[] args) {
+            
+            int x;
+            double y, consumption;
+
+            x = int.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+
+            consumption = x / y;
+
+            Console.WriteLine(consumption.ToString("F3") + " KM/L");
+
+        }
+    }
+}
