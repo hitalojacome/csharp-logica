f57fd1356684836b2dce3bc1554f52083a0a2cbf
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-18 20:15:19 -0300

    Idade em Dias

---
+++ uri1020/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uri1020 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n, years, months, days, remainder;
+
+            n = int.Parse(Console.ReadLine());
+
+            years = n / 365;
+            remainder = n % 365;
+            months = remainder / 30;
+            days = remainder % 30;
+
+            Console.WriteLine(years + " ano(s)");
+            Console.WriteLine(months + " mês(es)");
+            Console.WriteLine(days + " dia(s)");
+            
+        }
+    }
+}
