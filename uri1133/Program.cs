a7ec258301473888150f277dae47c1f059a7cb0a
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-18 09:53:15 -0300

    Resto da Divisão

---
+++ uri1133/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uri1133 {
+    class Program {
+        static void Main(string[] args) {
+
+            int x, y, aux;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y) {
+                aux = x;
+                x = y;
+                y = aux;
+            }
+
+            for (int i = x + 1; i < y; i++) {
+                if (i % 5 == 2 || i % 5 == 3) {
+                    Console.WriteLine(i);
+                }
+            }
+            
+        }
+    }
+}
