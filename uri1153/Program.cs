5f84d72f597af08c0f7e62ce6f087254782f90fd
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-18 12:10:56 -0300

    Fatorial Simples

---
+++ uri1153/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uri1153 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int fat = 1;
+            for (int i = 1; i <= n; i++) {
+                fat*= i;
+            }
+
+            Console.WriteLine(fat);
+            
+        }
+    }
+}
