88cb78899dd024c59f61ba5ee073221d185372a6
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-18 09:34:42 -0300

    Dividindo X por Y

---
+++ uri1116/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uri1116 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i =0; i < n; i++) {
+
+                string[] values = Console.ReadLine().Split(' ');
+                int x = int.Parse(values[0]);
+                int y = int.Parse(values[1]);
+
+                if (y == 0) {
+                    Console.WriteLine("divisao impossivel");
+                } else {
+                    double division = (double) x / y;
+                    Console.WriteLine(division.ToString("F1"));
+                }
+
+            }
+            
+        }
+    }
+}
