388a2323a0e160c96af8da005ba815e2cbd8271e
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-18 20:41:26 -0300

    Intervalo

---
+++ uri1037/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uri1037 {
+    class Program {
+        static void Main(string[] args) {
+
+            double n = double.Parse(Console.ReadLine());
+
+            if (n >= 0.0 && n <= 25.0) {
+                Console.WriteLine("Intervalo [0,25]");
+            } else if (n > 25.0 && n <= 50.0) {
+                Console.WriteLine("Intervalo (25,50]");
+            } else if (n > 50.1 && n <= 75.0) {
+                Console.WriteLine("Intervalo (50,75]");
+            } else if (n > 75.0 && n <= 100) {
+                Console.WriteLine("Intervalo (75,100]");    
+            } else {
+                Console.WriteLine("Fora de intervalo.");
+            }
+            
+        }
+    }
+}
