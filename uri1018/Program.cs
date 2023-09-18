818b65d4ec21f7e1a68d5f3f8590feb09bb49fe9
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-18 20:02:22 -0300

    Cédulas

---
+++ uri1018/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace uri1018 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n, quotient, remainder, note;
+
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n);
+
+            remainder = n;
+
+            note = 100;
+            quotient = remainder / note;
+            Console.WriteLine(quotient + " nota(s) de R$ " + note + ",00");
+            remainder %= note;
+
+            note = 50;
+            quotient = remainder / note;
+            Console.WriteLine(quotient + " nota(s) de R$ " + note + ",00");
+            remainder %= note;
+
+            note = 20;
+            quotient = remainder / note;
+            Console.WriteLine(quotient + " nota(s) de R$ " + note + ",00");
+            remainder %= note;
+
+            note = 10;
+            quotient = remainder / note;
+            Console.WriteLine(quotient + " nota(s) de R$ " + note + ",00");
+            remainder %= note;
+
+            note = 5;
+            quotient = remainder / note;
+            Console.WriteLine(quotient + " nota(s) de R$ " + note + ",00");
+            remainder %= note;
+
+            note = 2;
+            quotient = remainder / note;
+            Console.WriteLine(quotient + " nota(s) de R$ " + note + ",00");
+            remainder %= note;
+
+            Console.WriteLine(remainder + " nota(s) de R$ 1,00");
+
+        }
+    }
+}
