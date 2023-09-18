b1a174ddf552805085edb9275a826f88038c72a6
HITALO JACOME <hjacome@websupply.com.br>
2023-09-18 08:50:24 -0300

    Soma dos Ímpares Consecutivos II

---
+++ uri1099/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace uri1099 {
+    class Program {
+        static void Main(string[] args) {
+            
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++) {
+
+                string[] values = Console.ReadLine().Split(' ');
+                int x = int.Parse(values[0]);
+                int y = int.Parse(values[1]);
+
+                if (x > y) {
+                    int aux = x;
+                    x = y;
+                    y = aux;
+                }
+
+                int sum = 0;
+                for (int j = x + 1; j < y; j++) {
+                    if (j % 2 != 0) {
+                        sum = sum + j;
+                    }
+                }
+                Console.WriteLine(sum);
+            }
+
+        }
+    }
+}
