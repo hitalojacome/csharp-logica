ec68cdba61c6dc3a74f672a214bdd0f8cfed833a
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-18 10:17:53 -0300

    PUM

---
+++ uri1142/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uri1142 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int start = 1;
+            for (int i = 1; i <= n; i++) {
+
+                int second = start + 1;
+                int third = start + 2;
+                Console.WriteLine(start + " " + second + " " + third + " PUM");
+
+                start += 4;
+            }
+            
+        }
+    }
+}
