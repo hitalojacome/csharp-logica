0b2567bf58c80ba90fb4bc67c68e37ac4b6da5fd
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-18 20:07:13 -0300

    Conversão de Tempo

---
+++ uri1019/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace uri1019 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n, remainder, hours, minutes, seconds;
+
+            n = int.Parse(Console.ReadLine());
+
+            hours = n / 3600;
+            remainder = n % 3600;
+            minutes = remainder / 60;
+            seconds = remainder % 60;
+
+            Console.WriteLine(hours + ":" + minutes + ":" + seconds);
+            
+        }
+    }
+}
