d8ced435d106c5ea4913a44b06b6a3d15c09f08d
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-18 10:38:34 -0300

    Sequência Lógica

---
+++ uri1144/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace uri1144 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+
+                int second = i * i;
+                int third = i * i * i;
+                Console.WriteLine(i + " " + second + " " + third);
+                Console.WriteLine(i + " " + (second + 1) + " " + (third + 1));
+
+            }
+            
+        }
+    }
+}
