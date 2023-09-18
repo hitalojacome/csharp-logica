7442209cb7fbc3c110f3fbe2407d2b1f0dcdb653
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-18 12:32:26 -0300

    Sequência S

---
+++ uri1155/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uri1155 {
+    class Program {
+        static void Main(string[] args) {
+
+            double sum = 0;
+            for (int i = 1; i <= 100; i++) {
+                sum += 1.0 / i;
+            }
+
+            Console.WriteLine(sum.ToString("F2"));
+            
+        }
+    }
+}
