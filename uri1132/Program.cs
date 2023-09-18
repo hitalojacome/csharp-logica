65a966c1964c2ba0ca00dd67af841cbc933c4479
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-18 09:47:28 -0300

    Múltiplos de 13

---
+++ uri1132/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace uri1132 {
+    class Program {
+        static void Main(string[] args) {
+
+            int x, y, smaller, greater, sum;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            greater = y;
+            smaller = x;
+
+            if (x > y) {
+                greater = x;
+                smaller = y;
+            }
+
+            sum = 0;
+            for (int i = smaller; i <= greater; i++) {
+                if (i % 13 != 0) {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
