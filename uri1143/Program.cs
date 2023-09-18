bb59e0a7f26d97cb340cb54ea32c823afee288f2
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-18 10:31:38 -0300

    Quadrado e ao Cubo

---
+++ uri1143/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1143 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+
+                int first = i;
+                int second = i * i;
+                int third = i * i * i;
+                Console.WriteLine($"{first} {second} {third}");
+            }
+            
+        }
+    }
+}
