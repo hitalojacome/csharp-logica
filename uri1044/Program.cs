462ce594cdbff7cad452634769a0027c3d72e9e5
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-19 14:36:44 -0300

    Múltiplos

---
+++ uri1044/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1044 {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] values = Console.ReadLine().Split(' ');
+            int A = int.Parse(values[0]);
+            int B = int.Parse(values[1]);
+
+            if (A % B == 0 || B % A == 0) {
+                Console.WriteLine("Os números digitidos SÃO múltiplos");
+            } else {
+                Console.WriteLine("Os números digitidos NÃO são múltiplos");
+            }
+            
+        }
+    }
+}
