6f278d81f34ecf8989138d9bf32d0ccfa4592df8
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-19 16:02:55 -0300

    Tempo de Jogo

---
+++ uri1046/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uri1046 {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] hours = Console.ReadLine().Split(' ');
+            int initialHour = int.Parse(hours[0]);
+            int finalHour = int.Parse(hours[1]);
+
+            int duration;
+            if (initialHour < finalHour) {
+                duration = finalHour - initialHour;
+            } else {
+                duration = 24 - initialHour + finalHour;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duration + " HORA(S)");
+            
+        }
+    }
+}
