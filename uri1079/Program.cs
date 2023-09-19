bc64eb4f01db0fe8c7aa6aeef40495ea0d526207
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-19 16:54:02 -0300

    Médias Ponderadas

---
+++ uri1079/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uri1079 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine()); // número de casos
+
+            for (int i = 0; i < n; i++) {
+
+                string[] values = Console.ReadLine().Split(' ');
+                double value1 = double.Parse(values[0]);
+                double value2 = double.Parse(values[1]);
+                double value3 = double.Parse(values[2]);
+
+                double averageWeighted = (value1 * 2 + value2 * 3 + value3 * 5) / (2 + 3 + 5);
+
+                Console.WriteLine(averageWeighted.ToString("F1"));
+            }
+            
+        }
+    }
+}
