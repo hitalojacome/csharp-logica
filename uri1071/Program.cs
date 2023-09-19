70877fa7ef2b35ed531749c21ce45ca592f0e798
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-19 16:36:56 -0300

    Soma de Impares Consecutivos I

---
+++ uri1071/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace uri1071 {
+    class Program {
+        static void Main(string[] args) {
+
+            int x, y, min, max, sum;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x < y) {
+                min = x;
+                max = y;
+            } else {
+                min = y;
+                max = x;
+            }
+
+            sum = 0;
+            for (int i = min + 1; i < max; i++) {
+                if (i % 2 != 0) {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
