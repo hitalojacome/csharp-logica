f62f7f8cbc0c1cdf5133668d9d9253a3f1f05283
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-20 10:21:55 -0300

    Ex vetores 3

---
+++ vetores/vetores3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace vet3 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Quantos números deseja inserir? ");
+            int n = int.Parse(Console.ReadLine());
+            int[] A = new int[n];
+            int[] B = new int[n];
+
+            Console.Write("Insira os valores do primeiro vetor: ");
+            string[] valueA = Console.ReadLine().Split(' ');
+
+            Console.Write("Insira os valores do segundo vetor: ");
+            string[] valueB = Console.ReadLine().Split(' ');
+
+            for (int i=0; i<n; i++) {
+                A[i] = int.Parse(valueA[i]);
+                B[i] = int.Parse(valueB[i]);
+            }
+
+            int[] C = new int[n];
+            Console.Write("Segue a soma dos valores: ");
+            for (int i=0; i<n; i++) {
+                C[i] = A[i] + B[i];
+                Console.Write(C[i] + " ");
+            }
+            
+        }
+    }
+}
