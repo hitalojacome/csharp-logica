e5ab74be9614a91b818bcecce41b880f1506046a
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-20 10:28:59 -0300

    Ex vetores 5

---
+++ vetores/vetores5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace vet5 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Quantos números deseja inserir? ");
+            int n = int.Parse(Console.ReadLine());
+            double[] vet = new double[n];
+
+            Console.Write("Insira os valores do vetor: ");
+            string[] values = Console.ReadLine().Split(' ');
+
+            for (int i=0; i<n; i++) {
+                vet[i] = double.Parse(values[i]);
+            }
+
+            double sum = 0;
+            int cont = 0;
+            for (int i=0; i<n; i++) {
+                if (vet[i] % 2 == 0) {
+                    sum += vet[i];
+                    cont++;
+                }
+            } 
+
+            double avg = sum / cont;   
+            Console.WriteLine("Média dos valores pares: " + avg);
+            
+        }
+    }
+}
