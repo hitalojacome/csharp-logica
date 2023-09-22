3bcb533f05c79ddb6250f973f8d8a223a5fe4480
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-22 09:28:38 -0300

    Ex matriz 3

---
+++ matrizes/matriz3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace matriz3 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Insira a quantidade de linhas e colunas: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n,n];
+
+            for (int i=0; i<n; i++) {
+                Console.Write($"Insira os valores da {i+1}° Linha: ");
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j=0; j<n; j++) {
+                    mat[i,j] = int.Parse(values[j]); 
+                }
+            }
+
+            for (int i=0; i<n; i++) {
+                int higher = 0;
+                for (int j=0; j<n; j++) {
+                    if (mat[i,j] > higher) {
+                        higher = mat[i,j];
+                    }
+                }
+                Console.WriteLine($"Maior valor da {i+1}° linha: {higher}");
+            }
+            
+        }
+    }
+}
