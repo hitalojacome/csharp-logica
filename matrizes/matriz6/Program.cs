c56f88112d4568ac42b3f7b536ed1e70074873a1
Hitalo Jacome <hjacome@websupply.com.br>
2023-09-22 11:58:21 -0300

    Ex matriz 6

--- matrizes/matriz6/Program.cs
+++ matrizes/matriz6/Program.cs
@@ -27,11 +27,48 @@ namespace matriz6 {
             Console.WriteLine($"Soma dos valores positivos: {sum}");
 
             Console.Write("Qual linha deseja visualizar? ");
-            int chosenLine = int.Parse(Console.ReadLine());
+            int chosenLine = int.Parse(Console.ReadLine()) - 1;
+            for (int i=0; i<n; i++) {
+                if (chosenLine == i) {
+                    Console.Write($"Dados da {i+1}° linha: ");
+                    for (int j=0; j<n; j++) {
+                        Console.Write(mat[i,j].ToString("F1") + " ");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            Console.Write("Qual coluna deseja visualizar? ");
+            int chosenColumn = int.Parse(Console.ReadLine()) - 1;
+            Console.Write($"Dados da coluna desejada: ");
             for (int i=0; i<n; i++) {
                 for (int j=0; j<n; j++) {
-                    Console.Write
+                    if (chosenColumn == j) {
+                        Console.Write(mat[i,j].ToString("F1") + " ");
+                    }
                 }
+            }
+            Console.WriteLine();
+
+            Console.Write("Diagonal principal da matriz: ");
+            for (int i=0; i<n; i++) {
+                for (int j=0; j<n; j++) {
+                    if (i == j) {
+                        Console.Write(mat[i,j].ToString("F1") + " ");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Matriz alterada:");
+            for (int i=0; i<n; i++) {
+                for (int j=0; j<n; j++) {
+                    if (mat[i,j] < 0) {
+                        mat[i,j] *= mat[i,j]; 
+                    }
+                    Console.Write(mat[i,j].ToString("F1") + " ");
+                }
+                Console.WriteLine();
             }
             
         }
