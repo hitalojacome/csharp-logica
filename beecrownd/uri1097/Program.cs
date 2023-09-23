16dd9fb90f3f16ef617f4a43a585d22d3b2e4def
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-23 12:10:42 -0300

    Sequencia IJ 3

---
+++ beecrownd/uri1097/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uri1097
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int j = 7;
+            for (int i = 1; i <= 9; i += 2)
+            {
+                Console.WriteLine("I=" + i + " J=" + j);
+                Console.WriteLine("I=" + i + " J=" + (j - 1));
+                Console.WriteLine("I=" + i + " J=" + (j - 2));
+                j += 2;
+
+            }
+
+        }
+    }
+}
