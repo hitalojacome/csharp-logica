902b8e7be13a9be9c14d704dffa53cbea401ad78
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-23 11:57:06 -0300

    uri1080

---
+++ beecrownd/uri1080/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uri1080 {
+    class Program {
+        static void Main(string[] args) {
+
+            int x = 0; int highValue = 0; int highValuePosition = 0;
+            for (int i=0; i<10; i++) {
+                x = int.Parse(Console.ReadLine());
+
+                if (x > highValue) {
+                    highValue = x;
+                    highValuePosition = i;
+                }
+            }
+
+            Console.WriteLine($"Maior valor dos 10 inseridos foi {highValue}");
+            Console.WriteLine($"Posição dentre os 10: {highValuePosition+1}°");
+            
+        }
+    }
+}
